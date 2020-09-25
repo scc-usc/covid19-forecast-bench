2ce62ccd100d6694e05c1324fd58461ec9a9ce85
scc-usc <ajiteshs@acheron.usc.edu>
2020-09-24 21:28:59 -0700

    added script for data download

---
+++ scripts/add_to_data.m
@@ -0,0 +1,24 @@
+path = '../historical-data/';
+dirname = datestr(forecast_date, 'yyyy-mm-dd');
+
+fullpath = [path dirname '/'];
+
+if ~exist(fullpath, 'dir')
+    mkdir(fullpath);
+end
+
+src_address = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/';
+
+fname = 'time_series_covid19_confirmed_US.csv';
+urlwrite([src_address fname], [fullpath fname]);
+
+fname = 'time_series_covid19_deaths_US.csv';
+urlwrite([src_address fname], [fullpath fname]);
+
+fname = 'time_series_covid19_confirmed_global.csv';
+urlwrite([src_address fname], [fullpath fname]);
+
+fname = 'time_series_covid19_deaths_global.csv';
+urlwrite([src_address fname], [fullpath fname]);
+
+display(['Files written in ' fullpath ]);

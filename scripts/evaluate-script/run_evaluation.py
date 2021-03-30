80b922d78b7c9760604d4b2ddb664a3fac943f5f
Frost Tianjian Xu <xutianjian@gmail.com>
2021-03-29 21:19:24 -0700

    Update the evaluation script

--- scripts/evaluate-script/run_evaluation.py
+++ scripts/evaluate-script/run_evaluation.py
@@ -14,8 +14,8 @@ with open("forecasts_filenames.txt", "w") as f:
     for m in models:
         for csv in os.listdir("../../formatted-forecasts/state-death/" + m):
             date_num = (datetime.datetime.now() - datetime.datetime(2020, 1, 22)).days;
-            for i in range(4):
-                date_num -= i * 7
+            for i in range(32):
+                date_num -= 1
                 if "_{}.csv".format(date_num) in csv:
                     f.write(csv + '\n')
 
@@ -26,5 +26,5 @@ for directory in os.listdir("./output/"):
     shutil.rmtree("./output/{}".format(directory))
 
 # Clear txt files.
-open("models.txt").close()
-open("forecasts_filenames.txt").close()
+open("models.txt", 'w').close()
+open("forecasts_filenames.txt", 'w').close()

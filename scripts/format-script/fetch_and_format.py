6502b08b9016f029ea61d056dca4960767dd601d
Frost Tianjian Xu <xutianjian@gmail.com>
2021-03-31 15:36:08 -0700

    Refactor folder structure

--- scripts/format-script/fetch_and_format.py
+++ scripts/format-script/fetch_and_format.py
@@ -15,13 +15,13 @@ for i in range(TIME_SPAN):
     src_path = "../../../covid19-forecast-hub/data-processed/USC-SI_kJalpha/"
     for csv in os.listdir(src_path):
         if csv[0:10] == prefix:
-            shutil.copyfile(src_path+csv, "../../raw-forecasts/USC-SI_kJalpha/"+csv)
+            shutil.copyfile(src_path+csv, "../../raw-forecasts/US-COVID/USC-SI_kJalpha/"+csv)
 
 # From raw forecasts folder.
 for i in range(TIME_SPAN):
     prefix = (TODAY - datetime.timedelta(days=i)).strftime('%Y-%m-%d')
-    for model in os.listdir("../../raw-forecasts/"):
-        src_path = "../../raw-forecasts/" + model + '/'
+    for model in os.listdir("../../raw-forecasts/US-COVID/"):
+        src_path = "../../raw-forecasts/US-COVID/" + model + '/'
         if os.path.isdir(src_path):
             for csv in os.listdir(src_path):
                 if csv[0:10] == prefix:
@@ -35,22 +35,22 @@ for csv in os.listdir("./input/"):
     with open("county_case.txt", 'a') as f:
         f.write(csv + '\n')
 
-print("Fetch State Death Forecasts:")
+print("Fetch US State Death Forecasts:")
 exec(open("./fetch_forecasts_state_death.py").read())
 for model in os.listdir("./output/"):
-    merge("./output/"+model+'/', "../../formatted-forecasts/state-death/"+model+'/')
+    merge("./output/"+model+'/', "../../formatted-forecasts/US-COVID/state-death/"+model+'/')
     shutil.rmtree("./output/"+model)
 
 print("Fetch State Case Forecasts:")
 exec(open("./fetch_forecasts_state_case.py").read())
 for model in os.listdir("./output/"):
-    merge("./output/"+model+'/', "../../formatted-forecasts/state-case/"+model+'/')
+    merge("./output/"+model+'/', "../../formatted-forecasts/US-COVID/state-case/"+model+'/')
     shutil.rmtree("./output/"+model)
 
 print("Fetch County Case Forecasts:")
 exec(open("./fetch_forecasts_county_case.py").read())
 for model in os.listdir("./output/"):
-    merge("./output/"+model+'/', "../../formatted-forecasts/county-case/"+model+'/')
+    merge("./output/"+model+'/', "../../formatted-forecasts/US-COVID/county-case/"+model+'/')
     shutil.rmtree("./output/"+model)
 
 # Clean text files and input.
@@ -82,21 +82,21 @@ print("Fetch State Death Forecasts:")
 exec(open("./fetch_forecasts_state_death.py").read())
 os.system("./add_FH_prefix.sh")
 for model in os.listdir("./output/"):
-    merge("./output/"+model+'/', "../../formatted-forecasts/state-death/"+model+'/')
+    merge("./output/"+model+'/', "../../formatted-forecasts/US-COVID/state-death/"+model+'/')
     shutil.rmtree("./output/"+model)
 
 print("Fetch State Case Forecasts:")
 exec(open("./fetch_forecasts_state_case.py").read())
 os.system("./add_FH_prefix.sh")
 for model in os.listdir("./output/"):
-    merge("./output/"+model+'/', "../../formatted-forecasts/state-case/"+model+'/')
+    merge("./output/"+model+'/', "../../formatted-forecasts/US-COVID/state-case/"+model+'/')
     shutil.rmtree("./output/"+model)
 
 print("Fetch County Case Forecasts:")
 exec(open("./fetch_forecasts_county_case.py").read())
 os.system("./add_FH_prefix.sh")
 for model in os.listdir("./output/"):
-    merge("./output/"+model+'/', "../../formatted-forecasts/county-case/"+model+'/')
+    merge("./output/"+model+'/', "../../formatted-forecasts/US-COVID/county-case/"+model+'/')
     shutil.rmtree("./output/"+model)
 
 # Clean text files and input.
